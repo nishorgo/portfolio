53a9e3a8a88bc06caec01d2f04cf3f4b6a3cddb5
Abid Hasan <abidhasan.bh@outlook.com>
2024-09-18 00:23:16 +0600

    fix: encoded the apostrophe in a text

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -13,16 +13,21 @@ export const ContactSection = () => {
           <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create something amazing together
+                Let&apos;s create something amazing together
               </h2>
               <p className="text-sm md:text-base mt-2">
-                Ready to bring your next project to life? Let's connect and
+                Ready to bring your next project to life? Let&apos;s connect and
                 discuss how I can help you achieve your goals.
               </p>
             </div>
             <div>
               <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
-                <a href="mailto:abidhasan.bh@outlook.com" className="font-semibold">Contact Me</a>
+                <a
+                  href="mailto:abidhasan.bh@outlook.com"
+                  className="font-semibold"
+                >
+                  Contact Me
+                </a>
                 <ArrowUpRightIcon className="size-4" />
               </button>
             </div>
