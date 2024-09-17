8975f69300d1ec779b8d0e874206f83253ac3fea
Abid Hasan <abidhasan.bh@outlook.com>
2024-09-17 23:57:10 +0600

    refactor: added projects and images

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,49 +1,81 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
-import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import cocreateLandingPage from "@/assets/images/cocreate.png";
+import findnestLandingPage from "@/assets/images/findnest.png";
+import gamedbLandingPage from "@/assets/images/gamedb.png";
+import snakeMockup from "@/assets/images/snake.png";
+
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
-import Image from "next/image";
-import SectionHeader from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
+import SectionHeader from "@/components/SectionHeader";
+import Image from "next/image";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "FindNest",
+    year: "",
+    title: "Rent or buy homes",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "✦ RESTful endpoints for users, posts, notifications and chat" },
+      {
+        title:
+          "✦ Responsive interface for advanced search filters, property details, location map, real-time chat, etc.",
+      },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    code: "https://github.com/nishorgo/findnest",
+    image: findnestLandingPage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: "Co-Create",
+    year: "",
+    title: "A live collaboration platform",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      {
+        title:
+          "✦ Live collaboration between users, instantly reflecting changes for all participants",
+      },
+      {
+        title:
+          "✦ Can create, edit, and style shapes, text, and freeform drawings, with real-time chat and reactions",
+      },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
+    live: "https://co-create-board.vercel.app/",
+    code: "https://github.com/nishorgo/CoCreate",
+    image: cocreateLandingPage,
   },
   {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
+    company: "GameDB",
+    year: "",
+    title: "IMDb for video games",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      {
+        title:
+          "✦ RESTful endpoints for game data actions alongside user reviews and wishlists",
+      },
+      { title: "✦ Optimized API performance with Celery and Redis" },
+      {
+        title:
+          "✦ Built a responsive React frontend with Zustand for lightweight state management",
+      },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    live: "https://gamedb-indol.vercel.app/",
+    code: "https://github.com/nishorgo/GameDB",
+    image: gamedbLandingPage,
+  },
+  {
+    company: "chatty snake and ladders",
+    year: "",
+    title: "Online Snake and Ladders Game",
+    results: [
+      {
+        title:
+          "✦ Multiplayer Snake and Ladder game with real-time player interactions using socket programming",
+      },
+      {
+        title:
+          "✦ Built a client-server system for seamless game synchronization",
+      },
+    ],
+    code: "https://github.com/nishorgo/ChattySnakeLadders",
+    image: snakeMockup,
   },
 ];
 
@@ -62,7 +94,7 @@ export const ProjectsSection = () => {
               key={project.title}
               className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
               style={{
-                top: `calc(64px + ${projectIndex * 40}px)`
+                top: `calc(64px + ${projectIndex * 40}px)`,
               }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
@@ -82,17 +114,23 @@ export const ProjectsSection = () => {
                         key={result.title}
                         className="flex gap-2 text-sm md:text-base text-white/50"
                       >
-                        <CheckCircleIcon className="size-5 md:size-6" />
+                        {/* <CheckCircleIcon className="size-5 md:size-6" /> */}
                         <span>{result.title}</span>
                       </li>
                     ))}
                   </ul>
-                  <a href={project.link}>
-                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                  {project.live && <a href={project.live}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 mr-6">
                       <span>Visit Live Site</span>
                       <ArrowUpRightIcon className="size-4" />
                     </button>
-                  </a>
+                  </a>}
+                  {project.code && <a href={project.code}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>View Code</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>}
                 </div>
                 <div className="relative">
                   <Image
